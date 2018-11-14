c0b4862c27834ea190622f37fe3f15ca967180cc
rluker <rachellluker@gmail.com>
2018-11-13 18:32:43 -0700

    updated <body></body> for each html file

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 #don't leave spaces around the equals
 mkdir /var/www/html/A
 j="<a href="A/A.html">Open this!</a>"
-i="<html><title>Hello</title><body>Welcome! \n $j</body></html>"
+i="<html><title>Hello</title><body>Welcome to my page! \n $j</body></html>"
 echo $i > /var/www/html/index.html
-k="<html><title>Hello</title><body>Welcome! You made it to A.html.</body></html>"
+k="<html><title>Hello</title><body>Congrats! You made it to A.html.</body></html>"
 echo $k > /var/www/html/A/A.html
